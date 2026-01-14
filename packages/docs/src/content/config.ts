9cba57e6111cda1197121a1d5cfa6492fa1e326b
Alexander Karan <tech@alexanderkaran.com>
2026-01-14 17:36:35 +0800

    Added build stats

--- packages/docs/src/content/config.ts
+++ packages/docs/src/content/config.ts
@@ -4,9 +4,15 @@ const statsCollection = defineCollection({
   type: 'data',
   schema: z.object({
     name: z.string(),
+    type: z.string(),
     prodDependencies: z.number(),
     devDependencies: z.number(),
     npmGraphUrl: z.string().url().optional(),
+    installTimeMs: z.number(),
+    coldBuildTimeMs: z.number(),
+    warmBuildTimeMs: z.number(),
+    timingMeasuredAt: z.string(),
+    runner: z.string(),
   }),
 })
 
